00628473947e5ed7761e06e3ea2100d8ddabc4c1
Abhishek Gupta <Abhishekgupta862004@gmail.com>
2023-02-07 23:50:22 +0530

    Third commit to this file

---
+++ Arrays.java
@@ -0,0 +1,47 @@
+
+// class Calc{
+//     public int add(int nums[]){
+//         int sum = 0;
+//         for (int i = 0; i <= nums.length-1; i++){
+//             sum += nums[i];
+            
+//         }
+//         return sum;
+// }
+// }
+public class Arrays{
+    public static void main (String args[]){
+        // int a[] = new int[-5];
+        // System.out.println(a);
+        // Calc obj = new Calc();
+        // int nums[] = {5, 6, 7, 8,-5, 9};
+        // int result = obj.add(nums);
+        // System.out.println(result);
+        // int arr[] = new int [6];
+        // int arr[] = new int [5];
+
+        // System.out.print(arr[0]);
+
+        // int arr[] = {1, 2, 3, 4, 5};
+        // for ( int i = 0; i <= arr.length- 2; ++i){
+        //     System.out.println(arr[i] + " ");
+        // }
+            // int a1[] = new int [10];
+            // int a2[] = {1, 2, 3, 4, 5};
+            // System.out.println(a1.length + " " + a2.length);
+            // Object[] names = new String[3];
+            // names[0] = new Integer(0);
+
+            // int arr[] = new int [] {0, 1,2,3,4,5,6,7,8,9};
+
+            // int n = 6;
+            // n = arr[arr[n]/2];
+            // System.out.println(arr[n]/2);
+            int[] arr = new int[5];
+            arr = new int[6];
+
+        }
+
+    public static void sort(char[] first1) {
+    }
+}
---
+++ Assignment.java
@@ -0,0 +1,15 @@
+public class Assignment {
+
+    public static void Name ( int n){
+        // printing A....................
+
+        for ( int i = 0; i < n ; i++){
+            for ( int j = 0 ; j < n ; j++){
+                
+            }
+        }
+    }
+    public static void main ( String args[]){
+
+    }
+}
